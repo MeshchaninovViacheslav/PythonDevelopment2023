6c12954f7c68c62bde5f980cc6329d50dde7f026
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-03-06 17:38:13 +0300

    list_cows is done

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,12 +1,21 @@
 import cmd
+import shlex
+from cowsay import cowsay, list_cows, read_dot_cow
 
 
 class CowSay(cmd.Cmd):
     intro = "Moo!"
 
     def do_exit(self, arg):
-        "CowSay End"
+        """CowSay End"""
         return 1
+
+    def do_list_cows(self, arg):
+        """Lists all cow file names in the given directory"""
+        if not arg:
+            print(*list_cows())
+        else:
+            print(*list_cows(shlex.split(arg)[0]))
 
 
 if __name__ == "__main__":
