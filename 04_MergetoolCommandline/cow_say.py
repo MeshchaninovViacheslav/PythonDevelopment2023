6d7831fae8356d210e9e6d37d7bd77f2692d2a32
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-03-06 18:04:48 +0300

    make_bubble, bug fix

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,6 +1,6 @@
 import cmd
 import shlex
-from cowsay import cowsay, list_cows, read_dot_cow, make_bubble
+import cowsay
 
 
 class CowSay(cmd.Cmd):
@@ -13,16 +13,33 @@ class CowSay(cmd.Cmd):
     def do_list_cows(self, arg):
         """Lists all cow file names in the given directory"""
         if not arg:
-            print(*list_cows())
+            print(*cowsay.list_cows())
         else:
-            print(*list_cows(shlex.split(arg)[0]))
+            print(*cowsay.list_cows(shlex.split(arg)[0]))
 
     def do_make_bubble(self, arg):
         """
             Wraps text if wrap_text is true, then pads text and sets inside a bubble.
             This is the text that appears above the cows
         """
-        print(make_bubble(shlex.split(arg)[0]))
+        brackets = cowsay.THOUGHT_OPTIONS['cowsay']
+        width = 40
+        wrap_text = True
+
+        args = shlex.split(arg)
+        text = args.pop(0)
+        if args:
+            brackets = args.pop(0)
+        if args:
+            width = int(args.pop(0))
+        if args:
+            wrap_text = bool(args.pop(0))
+
+        print(cowsay.make_bubble(text, brackets, width, wrap_text))
+
+    # def do_cowthimaknk(self, arg):
+    #     """Returns the resulting cowthink string"""
+    #     print(cowthink(shlex.split(arg)[0]))
 
 
 if __name__ == "__main__":
