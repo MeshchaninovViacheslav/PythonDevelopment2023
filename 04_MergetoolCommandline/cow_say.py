242e7e4092076d321cea0a27b7e13c2b726fc6d5
Viacheslav Meshchaninov <meshchaninov.olymp@yandex.ru>
2023-03-06 21:11:37 +0300

    complete

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -13,10 +13,26 @@ def cowsay_parse(arg):
     if args:
         cow = args.pop(0)
     if args:
-        eyes = int(args.pop(0))
+        eyes = args.pop(0)
     if args:
-        tongue = bool(args.pop(0))
+        tongue = args.pop(0)
     return message, cow, eyes, tongue
+
+
+def complete(text, line, beg, end):
+    args = shlex.split(line)
+    idx = len(args) - 2
+    if beg == end:
+        idx += 1
+
+    data = {
+        "cows": cowsay.list_cows(),
+        "eyes": ["OO", "$$", "XX", "@@", "##"],
+        "tongues": ["U ", "UU", "MM", "WW", "VV"]
+    }
+
+    key = ["cows", "eyes", "tongues"][idx - 1]
+    return [c for c in data[key] if c.startswith(text)]
 
 
 class CowSay(cmd.Cmd):
@@ -54,12 +70,31 @@ class CowSay(cmd.Cmd):
         print(cowsay.make_bubble(text, brackets, width, wrap_text))
 
     def do_cowsay(self, arg):
+        """Returns the resulting cowsay string"""
         message, cow, eyes, tongue = cowsay_parse(arg)
         print(cowsay.cowsay(message=message, cow=cow, eyes=eyes, tongue=tongue))
 
     def do_cowthink(self, arg):
+        """Returns the resulting cowthink string"""
         message, cow, eyes, tongue = cowsay_parse(arg)
         print(cowsay.cowthink(message=message, cow=cow, eyes=eyes, tongue=tongue))
+
+    def complete_cowsay(self, text, line, beg, end):
+        """Autocomplete for cowsay"""
+        return complete(text, line, beg, end)
+
+    def complete_cowthink(self, text, line, beg, end):
+        """Autocomplete for cowsay"""
+        return complete(text, line, beg, end)
+
+    def complete_make_bubble(self, text, line, beg, end):
+        args = shlex.split(line)
+        idx = len(args) - 2
+        if beg == end:
+            idx += 1
+
+        if idx == 3:
+            return [c for c in ['True', 'False'] if c.lower().startswith(text.lower())]
 
 
 if __name__ == "__main__":
